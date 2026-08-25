ba096c887460ef074d46f26bad08033cd7bcaec0
GIOVANNI COSTA DE MEDEIROS <26171897@edu119.local>
2026-08-25 16:27:39 -0300

    bah

--- 06-poo/31-classe-curso.js
+++ 06-poo/31-classe-curso.js
@@ -5,3 +5,20 @@
 //
 // Escreva sua solução abaixo:
 
+class Curso {
+  constructor(nome, duracao, modalidade) {
+    this.nome = nome;             
+    this.duracao = duracao;       
+    this.modalidade = modalidade;
+  }
+
+  exibirInformacoes() {
+    console.log(`Curso: ${this.nome} - Duração: ${this.duracao} - Modalidade: ${this.modalidade}`);
+  }
+}
+
+const curso1 = new Curso("Análise e Desenvolvimento de Sistemas", "2.5 anos", "EAD");
+const curso2 = new Curso("Ciência da Computação", "4 anos", "Presencial");
+
+curso1.exibirInformacoes();
+curso2.exibirInformacoes();
