79c1069331966c634d1ad84d3a93fa3a0c0d2479
GIOVANNI COSTA DE MEDEIROS <26171897@edu119.local>
2026-08-25 15:57:29 -0300

    bah

--- 03-objetos/20-filtrar-objetos.js
+++ 03-objetos/20-filtrar-objetos.js
@@ -5,3 +5,14 @@
 //
 // Escreva sua solução abaixo:
 
+const Funcionários = [
+    {Nome: "Alex", Salario: 2000},
+    {Nome: "Biel", Salario: 1500},
+    {Nome: "DM", Salario: 580},
+    {Nome: "Didi", Salario: 99966600},
+    {Nome: "GAzborgh", Salario: 920837407860876},
+]
+
+const Menos3000 = Funcionários.filter(menos => menos.Salario >= 3000)
+
+console.log(Menos3000);
